65019dba7e5133ea33d3e2ffc9fc6630f21d806d
hassan-033 <99290640+hassan-033@users.noreply.github.com>
2022-03-21 20:44:58 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of variable.
+ * @s: string to be measured.
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int length;
+
+for (length = 0; s[length] != '\0'; length++)
+{
+}
+return (length);
+}
