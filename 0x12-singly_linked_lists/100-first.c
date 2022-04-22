18f79bd6a30a8953972ff1730cea119d1e634030
hassan-033 <99290640+hassan-033@users.noreply.github.com>
2022-04-22 10:29:20 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,9 +8,3 @@ void __attribute__((constructor)) premain()
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
