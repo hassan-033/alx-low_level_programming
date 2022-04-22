9d226d849ebe4e08742bfa03b6243a6e21f50c6d
hassan-033 <99290640+hassan-033@users.noreply.github.com>
2022-04-22 10:25:44 +0100

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * premain - print before main is called
+ */
+void __attribute__((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
