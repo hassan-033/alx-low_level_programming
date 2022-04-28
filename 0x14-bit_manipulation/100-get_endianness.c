44a1f958e578328c22bddc0d55c52914fbeff758
hassan-033 <99290640+hassan-033@users.noreply.github.com>
2022-04-28 14:55:03 +0100

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if multibyte data is stored as little or big endian
+ * Return: first value stored, 0 if big endian, 1 if little
+ */
+
+int get_endianness(void)
+{
+	int n = 0x00000001;
+	char *c = (char *)&n;
+
+	return (c[0]);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+S
